8a791d3bbba45c0ba10965b58b7a941d052f4b32
Manish <raimanish9894@gmail.com>
2024-02-26 22:56:48 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         <Stack direction="horizontal" gap="2" className="mb-4">
           <h1 className="me-auto">Budgets</h1>
           <Button variant="primary" onClick={() => setShowBudgetModal(true)}>
-            Add Budget
+            Add Budgets
           </Button>
           <Button variant="outline-primary" onClick={openAddExpenseModal}>
             Add Expense
